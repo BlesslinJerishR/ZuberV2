27c2e007fa571ebf9b2662af78583f515bf85841
Mastermindx33 <mastermindx33@gmail.com>
2021-11-02 09:49:12 +0530

    deploy @admin

---
+++ src/AdminDetails.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public class AdminDetails {
+    private String admin_name= "Blesslin";
+    private String admin_password= "zoho";
+    private ArrayList<Cab> cab_info= new ArrayList<Cab>();
+
+    public String get_admin_name(){
+        return admin_name;
+    }
+
+    public String get_admin_pass(){
+        return admin_password;
+    }
+}
